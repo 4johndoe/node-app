2f5f518fb324f898103e25c9d23b15d9abf0abc4
4johndoe <bogdandemchenko@gmail.com>
2016-03-27 23:14:35 +0300

    init commit

---
+++ client/components/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import NoteEditor from './NoteEditor.jsx';
+import NotesGrid from './NotesGrid.jsx';
+
+const App = React.createClass({
+	handleNoteAdd(data) {
+		console.log(data);
+	},
+
+	render() {
+		return (
+			<div className='App'>
+				<h2 className='App__header'>NotesApp</h2>
+				<NoteEditor onNoteAdd={this.handleNoteAdd} />
+				<NotesGrid />
+			</div>
+		);
+	}
+});
+
+export default App;
